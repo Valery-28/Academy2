0d74fe69e2ab2c632aa5a12eb41ae571b6cd8817
vpetc <valera9445@gmail.com>
2023-05-22 00:01:39 +0300

    re

---
+++ src/HomeWork1/Task3.java
@@ -0,0 +1,27 @@
+package HomeWork1;
+/*
+Задание 3
+Дан некоторый массив "int[] array", необходимо инвертировать его.
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int massiveRow = rand.nextInt(0, 20);
+        int[] array = new int[massiveRow];
+        for (int element = 0; element < array.length; element++) {
+            array[element] = rand.nextInt(0, 50);
+        }
+        System.out.println(Arrays.toString(array));
+
+        for (int element = 0; element < array.length / 2; element++) {
+            int temp = array[element];
+            array[element] = array[array.length - element-1];
+            array[array.length - element - 1] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
---
+++ src/HomeWork1/Task5.java
@@ -0,0 +1,37 @@
+package HomeWork1;
+/*
+Задание 5
+Дан некоторый двумерный массив "int[][] matrix". Нужно определить является ли он нижней
+треугольной матрицей или же нет.
+ */
+
+public class Task5 {
+    public static void main(String[] args) {
+        int fitstM;
+        int secondM;
+        int count;
+        int sum = 0;
+        int[][] matrix = {{1, 23, 34, 66, 45}, {0, 56, 45, 77, 34},
+                {0, 0, 5, 43, 65}, {0, 0, 0, 8, 35}, {0, 0, 0, 0, 21}};
+        for (fitstM = 0; fitstM < matrix.length; fitstM++) {
+            for (secondM = 0; secondM < matrix.length; secondM++) {
+                System.out.print(matrix[fitstM][secondM] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (fitstM = 1; fitstM < matrix.length; fitstM++) {
+            for (secondM = 0; secondM <= fitstM - 1; secondM++) {
+                System.out.print(matrix[fitstM][secondM] + " ");
+                count = matrix[fitstM][secondM];
+                sum += count;
+            }
+            System.out.println();
+        }
+        if (sum == 0) {
+            System.out.println("matrix treangulate");
+        } else {
+            System.out.println("matrix not treangulate");
+        }
+    }
+}
