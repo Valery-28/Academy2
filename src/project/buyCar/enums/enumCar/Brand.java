3fa53cc51742cb6f8f1f9dcb61009e0980ca4662
vpetc <valera9445@gmail.com>
2023-07-04 17:12:08 +0300

    Fix

--- src/project/buyCar/enums/enumCar/Brand.java
+++ src/project/buyCar/enums/enumCar/Brand.java
@@ -1,13 +1,13 @@
 package project.buyCar.enums.enumCar;
 
 public enum Brand {
-    AUDI(new CorolCar[]{CorolCar.BLACK,CorolCar.GREEN,},
+    AUDI(new CorolCar[]{CorolCar.BLACK,CorolCar.GREEN,CorolCar.WHITE},
             (new EngineCapacity[]{EngineCapacity.BIG,EngineCapacity.MAX}),
             (new SizeWheel[]{SizeWheel.R19,SizeWheel.R20})),
-    BMW(new CorolCar[]{CorolCar.BLUE,CorolCar.WHITE},
+    BMW(new CorolCar[]{CorolCar.BLUE,CorolCar.WHITE, CorolCar.ORANGE},
             (new EngineCapacity[]{EngineCapacity.STANDARD, EngineCapacity.MAX}),
             (new SizeWheel[]{SizeWheel.R17,SizeWheel.R18})),
-    FORD(new CorolCar[]{CorolCar.RED,CorolCar.PURPLE},
+    FORD(new CorolCar[]{CorolCar.RED,CorolCar.PURPLE, CorolCar.YELOW},
             (new EngineCapacity[]{EngineCapacity.SMALL, EngineCapacity.MIN}),
             (new SizeWheel[]{SizeWheel.R16,SizeWheel.R15}));
 
